e85acc958b707ff5d96e2f5b8264f36d047a99e8
Ankit Baliyan <a.baliyan008@gmail.com>
2023-12-02 16:53:42 +0530

    Update _bulk_file.py

--- code/version_2/_bulk_file.py
+++ code/version_2/_bulk_file.py
@@ -28,7 +28,7 @@ def main():
             'output': [""] * 4  
         })
     st.write(sample_df)
-    st.write("Note: Enter your data in Input column. \n Column name should be "input" and "output" ")
+    st.write("Note: Enter your data in Input column. \n Column name should be \"input\" and \"output\" ")
 
     # File uploader
     uploaded_file = st.file_uploader("Upload a file (CSV or Excel)", type=["csv", "xlsx"])
